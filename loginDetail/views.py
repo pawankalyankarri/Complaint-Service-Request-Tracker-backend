a56e8317acc58fb1b62dd04f20bbd62b39cd56b1
pawankalyan <pawankalyankarri22@gmail.com>
2025-08-14 22:23:24 +0530

    added department table

--- loginDetail/views.py
+++ loginDetail/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK,HTTP_400_BAD_REQUEST,HTTP_201_CREATED
-from .serializer import UserSerializer,TechSerializer,RequestSerializer
-from .models import UserLogin,TechLogin,Requests
+from .serializer import UserSerializer,TechSerializer,RequestSerializer,DepartmentsSerializer
+from .models import UserLogin,TechLogin,Requests,Departments
 # Create your views here.
 
 class LoginUser(APIView):
@@ -50,5 +50,21 @@ class RaiseRequests(APIView):
             return Response(status=HTTP_201_CREATED)
         else:
             return Response(ser_obj.errors,status=HTTP_400_BAD_REQUEST)
+        
+        
+class GetDepartments(APIView):
+    def get(self,req):
+        depts = Departments.objects.all()
+        ser_obj = DepartmentsSerializer(depts,many = True)
+        return Response(ser_obj.data,status=HTTP_200_OK)
+    
+    def post(self,req):
+        ser_obj = DepartmentsSerializer(data = req.data)
+        if ser_obj.is_valid():
+            ser_obj.save()
+            return Response(status=HTTP_200_OK)
+        else:
+            return Response(ser_obj.errors,status=HTTP_400_BAD_REQUEST)
+        
         
         
